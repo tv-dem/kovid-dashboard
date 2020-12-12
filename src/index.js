1f3310af968dca36eb1a3932cc6bed453ffbfaa2
Kotov Sergey <t9087620180@gmail.com>
2020-12-12 12:59:56 +0300

    add: add basic structure HTML

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
 // Test import of a JavaScript function
-import { example } from './js/example';
+// import { example } from './js/example';
 
 // Test import of an asset
-import webpackLogo from './images/webpack-logo.svg';
+// import webpackLogo from './images/webpack-logo.svg';
 
 // Test import of styles
 import './styles/index.scss';
 
 // Appending to the DOM
-const logo = document.createElement('img');
-logo.src = webpackLogo;
+// const logo = document.createElement('img');
+// logo.src = webpackLogo;
 
-const heading = document.createElement('h1');
+/// /const heading = document.createElement('h1');
 heading.textContent = example();
 
-const app = document.querySelector('#root');
-app.append(logo, heading);
+// const app = document.querySelector('#root');
+// app.append(logo, heading);
 
 const a = 1;
 console.log(a);
