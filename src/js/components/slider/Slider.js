9faac8345fd01f7da1529b52abd065e05fe01ba6
funfordima <d.g.litvinov@gmail.com>
2020-12-14 23:14:52 +0200

    feat: add class Slider.js for scrollbar

---
+++ src/js/components/slider/Slider.js
@@ -0,0 +1,55 @@
+import '../../../styles/graph.scss';
+import UI from '../UI/UI';
+
+export default class Slider extends UI {
+  constructor(parentSelector, slidesToScroll = 1, slidesToShow = 3) {
+    super();
+    this.parent = document.querySelector(parentSelector);
+    this.slidesToScroll = slidesToScroll;
+    this.slidesToShow = slidesToShow;
+    this.pageCounter = 0;
+  }
+
+  init(dataLabels) {
+    this.scrollContainer = super.render(this.parent, 'div', null, ['class', 'scroll__container']);
+    this.scrollLeftArrow = super.render(this.scrollContainer, 'div', null, ['class', 'scroll__left-arrow']);
+    this.leftArrow = super.render(this.scrollLeftArrow, 'img', null, ['src', ''], ['alt', '']);
+    this.scrollNav = super.render(this.scrollContainer, 'div', null, ['class', 'scroll__nav']);
+    this.scrollTrack = super.render(this.scrollNav, 'div', null, ['class', 'scroll__track']);
+    const itemWidth = 100 / this.slidesToShow;
+    // Render item with required width
+    dataLabels.forEach((labelName) => super.render(this.scrollTrack, 'div', labelName).style.minWidth = `${itemWidth}%`);
+    this.scrollRightArrow = super.render(this.scrollContainer, 'div', null, ['class', 'scroll__right-arrow']);
+    this.rightArrow = super.render(this.scrollRightArrow, 'img', null, ['src', ''], ['alt', '']);
+    this.movePosition = this.slidesToScroll * this.scrollTrack.querySelector('div').clientWidth;
+
+    // Handle event
+    this.scrollLeftArrow.addEventListener('click', this.clickBtnLeftHandler.bind(this, this.scrollTrack, this.movePosition));
+    // Вопрос Андрею: Как правильно снимать события в классе?
+    this.scrollRightArrow.addEventListener('click', this.clickBtnRightHandler.bind(this, this.scrollTrack, this.movePosition, dataLabels.length - this.slidesToShow));
+  }
+
+  clickBtnLeftHandler(track, movePosition) {
+    if (this.pageCounter > 0) {
+      this.pageCounter -= 1;
+    }
+
+    track.style.transform = `translateX(${-movePosition * this.pageCounter}px)`;
+
+    if (this.pageCounter === 0) {
+      return;
+    }
+  }
+
+  clickBtnRightHandler(track, movePosition, count) {
+    if (this.pageCounter < count) {
+      this.pageCounter += 1;
+    }
+
+    track.style.transform = `translateX(${-movePosition * this.pageCounter}px)`;
+
+    if (this.pageCounter === count) {
+      return;
+    }
+  }
+}
