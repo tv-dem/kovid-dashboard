8eada783f7260e51541818d5927e848147ca9041
sovanmarat <maratwp@mail.ru>
2020-12-14 14:40:18 +0300

    feat: added connector to api

--- src/js/components/connector/Connector.js
+++ src/js/components/connector/Connector.js
@@ -1,17 +1,43 @@
+import { URL_STATISTICS} from '../constants/constants';
+
 export class Connector {
-    constructor(url, token) {
-        this.URL = url;
-        this.token = token;
+    constructor(url) {
+        this.url = url;
+    }
+    async getStatistics() {
+        try {
+            const resultTemp = await fetch(url);
+            const result = await resultTemp.json();
+            return result;
+        }
+        catch (err) {
+            alert(err);
+        }
     }
 
-    getLink(params) {
-        //...
-        let link = '';
-        return link;        
-    }
 
-    getData(params) {
-        const link = this.getLink(params);
-        // async await fetch 
-    }
-}
+// result.Countries[i].CountryCode
+//https://www.countryflags.io/be/shiny/64.png
+
+
+//"Countries": По конкретной стране:
+
+// "Country": "Afghanistan", === страна
+// "CountryCode": "AF",  ===     код страны (необходим для получения флага)
+// "Slug": "afghanistan",
+// "NewConfirmed": 113, === случаи заражения за последние отчетные сутки
+// "TotalConfirmed": 48229, === общее количество зараженных
+// "NewDeaths": 11, === количество умерших за последние отчетные сутки
+// "TotalDeaths": 1956, === общее количество умерших
+// "NewRecovered": 59, === количество выздоровевших за последние отчетные сутки
+// "TotalRecovered": 38200, === общее количество выздоровевших
+
+
+// "Global": В МИРЕ:
+
+//     "NewConfirmed" === случаи заражения за последние отчетные сутки
+//     "TotalConfirmed" === общее количество зараженных
+//     "NewDeaths" === количество умерших за последние отчетные сутки
+// "TotalDeaths" === общее количество умерших
+// "NewRecovered" === количество выздоровевших за последние отчетные сутки
+// "TotalRecovered" === общее количество выздоровевших
---
+++ src/js/components/constants/constants.js
@@ -0,0 +1,1 @@
+export const URL_STATISTICS = 'https://api.covid19api.com/summary';
