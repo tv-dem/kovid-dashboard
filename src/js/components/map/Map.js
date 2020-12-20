b116f799c8e6bf0b154a369f90a0c0ce9b2c5cd9
Kotov Sergey <t9087620180@gmail.com>
2020-12-20 17:54:31 +0300

    fix: maps block

--- src/js/components/map/Map.js
+++ src/js/components/map/Map.js
@@ -1,3 +1,4 @@
+import ymaps from 'ymaps';
 import UI from '../UI/UI';
 
 export default class Map extends UI {
@@ -5,6 +6,14 @@ export default class Map extends UI {
     super();
     this.datalist = null;
     this.data = null;
+    this.contextMap = null;
+    this.highlightedDistrict = null;
+    this.districtBalloon = null;
+    this.ymaps = null;
+    this.districtCollections = {};
+    this.bordersInfo = {};
+    this.bordersFeatures = {}; // { код страны: feature от this.ymaps.borders.load }
+    this.countryIndexes = {}; // { код страны: индекс страны в yandex geoObjects }
   }
 
   init(datalist, globalData) {
@@ -18,119 +27,132 @@ export default class Map extends UI {
     this.data = data;
   }
 
-  initMap() {
-    let myMap;
+  createYMaps() {
+    this.contextMap = new this.ymaps.Map('map', {
+      center: [34.41, 0],
+      zoom: 2,
+      autoFitToViewport: 'always',
+      controls: ['zoomControl'],
+    });
+  }
 
-    function init() {
-      // Создание карты.
-      myMap = new ymaps.Map('map', {
-        // Координаты центра карты.
-        // Порядок по умолчанию: «широта, долгота».
-        // Чтобы не определять координаты центра карты вручную,
-        // воспользуйтесь инструментом Определение координат.
-        center: [34.41, 0],
-        // Уровень масштабирования. Допустимые значения:
-        // от 0 (весь мир) до 19.
-        zoom: 2,
-        autoFitToViewport: 'always',
-        controls: ['zoomControl'],
+  createStaticPane(map) {
+    return new this.ymaps.pane.StaticPane(map, {
+      zIndex: 100,
+      css: {
+        width: '100%',
+        height: '100%',
+        backgroundColor: '#f7f7f7',
+        opacity: '0.5',
+      },
+    });
+  }
 
-      });
+  createGeoObjectCollection(color) {
+    return new this.ymaps.GeoObjectCollection(null, {
+      fillColor: color,
+      strokeColor: color,
+      strokeOpacity: 0.3,
+      fillOpacity: 0.3,
+      hintCloseTimeout: 0,
+      hintOpenTimeout: 0,
+    });
+  }
 
-      const pane = new ymaps.pane.StaticPane(myMap, {
-        zIndex: 100,
-        css: {
-          width: '100%', height: '100%', backgroundColor: '#f7f7f7', opacity: '0.5',
-        },
-      });
-      myMap.panes.append('white', pane);
+  createDistrictBalloon() {
+    this.districtBalloon = new this.ymaps.Balloon(this.contextMap);
+  }
 
-      const districtBalloon = new ymaps.Balloon(myMap);
-      districtBalloon.options.setParent(myMap.options);
-      // Загрузим регионы.
-      ymaps.borders.load('001', {
-        lang: 'ru',
-        quality: 0,
-      }).then((result) => {
-        console.log('1', result);
+  static mouseEnterHandler(event) {
+    const district = event.get('target').getParent();
+    district.options.set({ fillOpacity: 1 });
+  }
 
-        // Создадим объект, в котором будут храниться коллекции с нашими регионами.
-        const districtCollections = {};
-        // Для каждого федерального округа создадим коллекцию.
-        let i = 1;
-        result.features.forEach((feature) => {
-          i++;
-          // var iso = feature.properties.iso3166;
-          districtCollections[feature.properties.iso3166] = new ymaps.GeoObjectCollection(null, {
-            fillColor: `rgb(${i + 1}, 100, 0)`,
-            strokeColor: `rgb(${i + 1}, 100, 0)`,
-            strokeOpacity: 0.3,
-            fillOpacity: 0.3,
-            hintCloseTimeout: 0,
-            hintOpenTimeout: 0,
-          });
-          // Создадим свойство в коллекции, которое позже наполним названиями субъектов РФ.
-          districtCollections[feature.properties.iso3166].properties.districts = [];
-        });
+  mouseLeaveHandler(event) {
+    const district = event.get('target').getParent();
+    if (district !== this.highlightedDistrict) {
+      district.options.set({ fillOpacity: 0.3 });
+    }
+  }
 
-        console.log('2', districtCollections);
+  clickHandler(event) {
+    const target = event.get('target');
+    const district = target.getParent();
 
-        result.features.forEach((feature) => {
-          const iso = feature.properties.iso3166;
-          const { name } = feature.properties;
-          // var district = feature.districtByIso[iso];
-          // debugger;
-          // console.log(iso, name);
-          //  Для каждого субъекта РФ зададим подсказку
-          feature.properties.hintContent = name;
-          //  Добавим субъект РФ в соответствующую коллекцию.
-          districtCollections[iso].add(new ymaps.GeoObject(feature));
-          //  Добавим имя субъекта РФ в массив.
-          districtCollections[iso].properties.districts.push(name);
-        });
-        console.log('333', districtCollections);
-        // console.log('444', ttt);
-
-        // Создадим переменную, в которую будем сохранять выделенный в данный момент федеральный округ.
-        let highlightedDistrict;
-        for (const districtName in districtCollections) {
-          // Добавим коллекцию на карту.
-          myMap.geoObjects.add(districtCollections[districtName]);
-
-          // При наведении курсора мыши будем выделять федеральный округ.
-          districtCollections[districtName].events.add('mouseenter', (event) => {
-            const district = event.get('target').getParent();
-            district.options.set({ fillOpacity: 1 });
-          });
-
-          // При выводе курсора за пределы объекта вернем опции по умолчанию.
-          districtCollections[districtName].events.add('mouseleave', (event) => {
-            const district = event.get('target').getParent();
-            if (district !== highlightedDistrict) {
-              district.options.set({ fillOpacity: 0.3 });
-            }
-          });
-          // Подпишемся на событие клика.
-          districtCollections[districtName].events.add('click', (event) => {
-            const target = event.get('target');
-            const district = target.getParent();
-            // Если на карте выделен федеральный округ, то мы снимаем с него выделение.
-            if (highlightedDistrict) {
-              highlightedDistrict.options.set({ fillOpacity: 0.3 });
-            }
-            // Откроем балун в точке клика. В балуне будут перечислены регионы того федерального округа,
-            // по которому кликнул пользователь.
-            districtBalloon.open(event.get('coords'), district.properties.districts.join('<br>'));
-            // Выделим федеральный округ.
-            district.options.set({ fillOpacity: 1 });
-            // Сохраним ссылку на выделенный федеральный округ.
-            highlightedDistrict = district;
-          });
-        }
-      });
+    if (this.highlightedDistrict) {
+      this.highlightedDistrict.options.set({ fillOpacity: 0.3 });
     }
 
-    ymaps.ready(init);
+    this.districtBalloon.open(event.get('coords'), district.properties.districts.join('<br>'));
+    district.options.set({ fillOpacity: 1 });
+    this.highlightedDistrict = district;
+  }
+
+  createDistrictCollections(feature, color) {
+    const { name, iso3166: iso } = feature.properties;
+    feature.properties.hintContent = name;
+    this.districtCollections[iso] = this.createGeoObjectCollection(color);
+    this.districtCollections[iso].properties.districts = [];
+
+    this.districtCollections[iso].add(new this.ymaps.GeoObject(feature));
+    this.districtCollections[iso].properties.districts.push(name);
+
+    this.districtCollections[iso].events.add('mouseenter',
+      (event) => Map.mouseEnterHandler(event));
+
+    this.districtCollections[iso].events.add('mouseleave',
+      (event) => this.mouseLeaveHandler(event));
+
+    this.districtCollections[iso].events.add('click',
+      (event) => this.clickHandler(event));
+  }
+
+  async createBordersInfo() {
+    const { features } = await this.ymaps.borders.load('001', {
+      lang: 'ru',
+      quality: 0,
+    });
+    this.bordersFeatures = features;
+    this.bordersFeatures.forEach((feature) => {
+      const { iso3166: iso } = feature.properties;
+      this.bordersInfo[iso] = feature;
+    });
+  }
+
+  updateDistricCollections(iso, color) {
+    this.createDistrictCollections(this.bordersInfo[iso], color);
+    this.contextMap.geoObjects.set(this.countryIndexes[iso],
+      this.districtCollections[iso]);
+  }
+
+  async initYandexMap() {
+    this.createYMaps();
+    const pane = this.createStaticPane(this.contextMap);
+    this.contextMap.panes.append('white', pane);
+    this.createDistrictBalloon();
+    this.districtBalloon.options.setParent(this.contextMap.options);
+    await this.createBordersInfo();
+
+    let red = 1;
+    this.bordersFeatures.forEach((feature) => {
+      red++;
+      const color = `rgb(${red + 1}, 0, 0)`;
+      this.createDistrictCollections(feature, color);
+    });
+
+    const districtList = Object.keys(this.districtCollections);
+    districtList.forEach((districtName, index) => {
+      this.contextMap.geoObjects.add(this.districtCollections[districtName]);
+      this.countryIndexes[districtName] = index;
+    });
+  }
+
+  async initMap() {
+    this.ymaps = await ymaps.load('https://api-maps.yandex.ru/2.1/?apikey=d79b2dc6-c925-42e8-a5be-459618c5977a&lang=ru_RU');
+    await this.initYandexMap();
+
+    // Метод для обновления цвета
+    this.updateDistricCollections('RU', 'rgb(3, 133, 7)');
   }
 
   renderMap() {
