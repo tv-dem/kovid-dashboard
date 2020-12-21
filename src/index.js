2bfbdc55dcd02f5648bfff4d4105d64b9020e789
sovanmarat <maratwp@mail.ru>
2020-12-21 21:39:55 +0300

    feat: added method clickBtn, setCountry, fixed index

--- src/index.js
+++ src/index.js
@@ -10,73 +10,15 @@ import { URL_STATISTICS, URL_FLAGS_POPULATION } from './js/components/constants/
 
 const init = async () => {
   const data = await Connector.getStatistics(URL_STATISTICS);
+  const dataPopulation = await Connector.getStatistics(URL_FLAGS_POPULATION);
   const list = new List(data.Countries, data.Global);
   list.renderComponent(document.querySelector('#country_list'));
-  const dataPopulation = await Connector.getStatistics(URL_FLAGS_POPULATION);
 
   // StatisticsView
   const blockStatistics = new StatisticsView(data.Global);
-  blockStatistics.render();
-  const allBtn = document.querySelectorAll('.all_btn');
-  const absoluteBtn = document.querySelectorAll('.absolute_btn');
-  const imgWorldBtn = document.querySelector('.world_right');
-  imgWorldBtn.addEventListener('click', () => {
-    blockStatistics.isCountry = false;
-    blockStatistics.setNewValue(data.Global);
-    blockStatistics.render();
-  });
-  let nameCountry = '';
-  let res = {};
-  let resPopulation = {};
+  blockStatistics.init();
 
-  // function clickCountry(el) {
-  //   blockStatistics.isCountry = true;
-  //   nameCountry = el.currentTarget.textContent.replace(/[0-9]/g, '');
-  //   console.log(nameCountry);
-  //   return population.find(({name}) => name === nameCountry);
-  //   res = data.Countries.find((base) => base.Country === nameCountry);
-  //   resPopulation = dataPopulation.find((base) => base.name === nameCountry);
-  //   blockStatistics.setNewValue(res, resPopulation);
-  //   blockStatistics.render();
-  // }
-  const listLi = document.querySelectorAll('.list__li');
-  listLi.forEach((e) => {
-    e.addEventListener('click', (el)=>{
-      blockStatistics.isCountry = true;
-      nameCountry = el.currentTarget.textContent.replace(/[0-9]/g, '');
-     // return population.find(({name}) => name === nameCountry);
-      res = data.Countries.find(({Country}) => Country === nameCountry);
-      resPopulation = dataPopulation.find(({name}) => name === nameCountry);
-      blockStatistics.setNewValue(res, resPopulation);
-      blockStatistics.render();
-    });
-  });
 
-  allBtn.forEach((el) => {
-    el.addEventListener('click', () => {
-      blockStatistics.isOneDay = !blockStatistics.isOneDay;
-      if (!blockStatistics.Country) {
-        blockStatistics.setNewValue(data.Global);
-      } else {
-        blockStatistics.setNewValue(res);
-      }
-      blockStatistics.render();
-    });
-  });
-  absoluteBtn.forEach((e) => {
-    e.addEventListener('click', () => {
-      blockStatistics.isHundredK = !blockStatistics.isHundredK;
-      if (!blockStatistics.Country) {
-        blockStatistics.setNewValue(data.Global);
-      } else {
-        blockStatistics.setNewValue(res);
-      }
-
-      blockStatistics.render();
-    });
-  });
-
-  // end StatisticsView
 };
 
 init();
--- src/js/components/statisticsView/StatisticsView.js
+++ src/js/components/statisticsView/StatisticsView.js
@@ -1,17 +1,21 @@
 export default class StatisticsView {
 
     constructor(
-    dataStatistics, 
-    dataPopulation,
-    isCountry = false,
-    isOneDay = false,
-    isHundredK = false
-        ) {
-
+        dataStatistics,
+        dataPopulation,
+        isCountry = false,
+        isOneDay = false,
+        isHundredK = false
+    ) {
+        this.nameCountry = null;
+        this.res = {};
+        this.resPopulation = {};
         this.setNewValue(dataStatistics, dataPopulation);
-        this.isCountry=isCountry;
-        this.isOneDay=isOneDay;
-        this.isHundredK=isHundredK;
+        this.isCountry = isCountry;
+        this.isOneDay = isOneDay;
+        this.isHundredK = isHundredK;
+        this.dataStatistics = dataStatistics;
+        this.dataPopulation = dataPopulation;
     }
 
     setNewValue(dataStatistics, dataPopulation) {
@@ -31,18 +35,55 @@ export default class StatisticsView {
             this.flag = dataPopulation.flag;
             this.name = dataPopulation.name;
         }
+
+
     };
 
+    setCountry(country) { // название страны, а не код страны
+        this.isCountry = true;
+        this.nameCountry = country.textContent.replace(/[0-9]/g, '');
+        this.res = this.dataStatistics.Countries.find(({ Country }) => Country === this.nameCountry);
+        this.resPopulation = this.dataPopulation.find(({ name }) => name === this.nameCountry);
+        this.setNewValue(this.res, this.resPopulation);
+        this.render();
+    }
+
+    clickBtn() {
+        const allBtn = document.querySelectorAll('.all_btn');
+        const absoluteBtn = document.querySelectorAll('.absolute_btn');
+        const imgWorldBtn = document.querySelector('.world_right');
+
+        allBtn.forEach((el) => {
+            el.addEventListener('click', () => {
+                this.isOneDay = !this.isOneDay;
+                this.Country ? this.setNewValue(this.res) : this.setNewValue(this.dataStatistics);
+                this.render();
+            });
+        });
+
+        absoluteBtn.forEach((e) => {
+            e.addEventListener('click', () => {
+                this.isHundredK = !this.isHundredK;
+                this.Country ? this.setNewValue(this.res) : this.setNewValue(this.dataStatistics);
+                this.render();
+            });
+        });
+        imgWorldBtn.addEventListener('click', () => {
+          this.isCountry = false;
+          this.setNewValue(this.dataStatistics);
+          this.render();
+        });
+    };
 
     render() {
-    const worldContent = document.querySelector('.world_content'),
-        allContent = document.querySelector('.all_content'),
-        absoluteContent = document.querySelector('.absolute_content'),
-        confirmedContent = document.querySelector('.informStatistics_confirmed_content'),
-        deathsContent = document.querySelector('.informStatistics_deaths_content'),
-        recoveredContent = document.querySelector('.informStatistics_recovered_content'),
-        globalCases = document.querySelector('.global_cases'),
-        worldRight = document.querySelector('.world_right');
+        const worldContent = document.querySelector('.world_content'),
+            allContent = document.querySelector('.all_content'),
+            absoluteContent = document.querySelector('.absolute_content'),
+            confirmedContent = document.querySelector('.informStatistics_confirmed_content'),
+            deathsContent = document.querySelector('.informStatistics_deaths_content'),
+            recoveredContent = document.querySelector('.informStatistics_recovered_content'),
+            globalCases = document.querySelector('.global_cases'),
+            worldRight = document.querySelector('.world_right');
 
 
         let contentConfirmed = '',
@@ -59,7 +100,7 @@ export default class StatisticsView {
 
         if (this.isCountry) {
             styleTitle = `${this.Country} <img src="${this.flag}" alt="flag" width="40px" height="30px" class ="imgFlag">`;
-            rightWorld=`<img src="https://raw.githubusercontent.com/SovanMarat/game_img/main/3/world2.png" alt="flag" width="45px" height="35px" class ="imgWorld imgWorld_link">`;
+            rightWorld = `<img src="https://raw.githubusercontent.com/SovanMarat/game_img/main/3/world2.png" alt="flag" width="45px" height="35px" class ="imgWorld imgWorld_link">`;
         }
         else styleTitle = `World <img src="https://raw.githubusercontent.com/SovanMarat/game_img/main/3/world2.png" alt="flag" width="45px" height="35px" class ="imgWorld">`;
         //`<img src="../../../public/arrow-left.svg" alt="flag" width="20px">`;
@@ -103,7 +144,11 @@ export default class StatisticsView {
         absoluteContent.innerHTML = contentAbsolute;
         worldRight.innerHTML = rightWorld;
 
-     }
-   
 
+    }
+
+    init(){
+        this.render();
+        this.clickBtn();
+    }
 }
